53d0a3dc372bdbc983b6acd033abdb3274e668e7
MarcioHideo <marcinishikawa@gmail.com>
2021-11-22 18:53:22 -0300

    Fix new forms to INSERT in tables

--- testes.sql
+++ testes.sql
@@ -1,18 +1,7 @@
 -- DROP TABLE individuo,empresa,processo_judicial,julgamento,cargo,partido,candidatura,pleito,doacao,doacao_juridica,equipe,membro_equipe
 -- DROP TYPE cargo_tipo, local_tipo
--- INSERT INTO individuo VALUES('cpf','nome',data_nascimento,fichaLimpa)
--- INSERT INTO empresa VALUES('cnpj','nome')
--- INSERT INTO processo_judicial(crime,procedente) VALUES('crime',boolean)
--- INSERT INTO julgamento VALUES('individuo',num_processo,data)
--- INSERT INTO cargo VALUES(cargo_tipo,cadeiras,local,tipoLocal,salario)
--- INSERT INTO partido VALUES('nome', 'programa')
--- INSERT INTO candidatura VALUES('candidato','vice',ano,cargo,local,tipoLocal,partido)
--- INSERT INTO pleito VALUES(ano,candidatura,num_votos)
--- INSERT INTO doacao VALUES(valor,'doador','candidato',ano)
--- INSERT INTO doacao_juridica VALUES(valor,'doador','candidato',ano)
--- INSERT INTO membro_equipe VALUES('membro','candidatura',ano)
 
---individuos
+-- INSERT INTO individuo VALUES('cpf','nome',data_nascimento,fichaLimpa)
 INSERT INTO individuo VALUES('81387911600','Manuel Manual','1990-12-09',true); --Cand_pres_PI
 INSERT INTO individuo VALUES('22126173135','Ednaldo Pereira','1970-09-02',true);--Cand_pres_PJ
 INSERT INTO individuo VALUES('12750152470','Eduardo Santos','2000-10-23',true);--Cand_pres_PP
@@ -36,35 +25,32 @@ INSERT INTO individuo VALUES('22364784964','Priscila Lima','1968-08-19',false);-
 INSERT INTO individuo VALUES('42425489150','Henrique Oliveira','1964-12-29',false);--ficha_suja
 INSERT INTO individuo VALUES('44216533302','Raysa Gomes','1986-04-16',false);--ficha_suja
 
-
-
-
---empresas
+-- INSERT INTO empresa VALUES('cnpj','nome')
 INSERT INTO empresa VALUES('23178074000118','Ford');
 INSERT INTO empresa VALUES('95896247000144','Sony');
 INSERT INTO empresa VALUES('90714175000153','Facebook');
 INSERT INTO empresa VALUES('89236358000121','Google');
 INSERT INTO empresa VALUES('90043287000120','Tesla');
 
---processo_judicial
-INSERT INTO processo_judicial(crime,procedente) VALUES('Homicidio ocorrido em 9 de Julho de 2000 em ...',true);
-INSERT INTO processo_judicial(crime,procedente) VALUES('Corrupcao, gastou 1000L de gasolina no corsa.',false);
-INSERT INTO processo_judicial(crime,procedente) VALUES('Corrupcao, usou o dinheiro em leite condensado.',true);
-INSERT INTO processo_judicial(crime,procedente) VALUES('Assalto o banco.',false);
-INSERT INTO processo_judicial(crime,procedente) VALUES('Assalto a padaria.',true);
+--INSERT INTO processo_judicial(crime) VALUES('crime')
+INSERT INTO processo_judicial(crime) VALUES('Homicidio ocorrido em 9 de Julho de 2000 em ...');
+INSERT INTO processo_judicial(crime) VALUES('Corrupcao, gastou 10 mil reais do dinheiro público em gasolina no corsa.');
+INSERT INTO processo_judicial(crime) VALUES('Corrupcao, usou o dinheiro público em leite condensado.');
+INSERT INTO processo_judicial(crime) VALUES('Assaltou o banco.');
+INSERT INTO processo_judicial(crime) VALUES('Assaltou a padaria.');
 
---julgamento
-INSERT INTO julgamento VALUES('44216533302',1,'2000-10-12');
-INSERT INTO julgamento VALUES('42425489150',2,'2020-12-11');
-INSERT INTO julgamento VALUES('22364784964',3,'2017-03-28');
-INSERT INTO julgamento VALUES('39031644293',4,'2018-09-05');
-INSERT INTO julgamento VALUES('83784243673',5,'2020-01-11');
+--INSERT INTO julgamento VALUES('cpf',num_processo,procedente,data);
+INSERT INTO julgamento VALUES('44216533302',1,true,'2000-10-12');
+INSERT INTO julgamento VALUES('42425489150',2,true,'2020-12-11');
+INSERT INTO julgamento VALUES('22364784964',3,true,'2017-03-28');
+INSERT INTO julgamento VALUES('39031644293',4,true,'2018-09-05');
+INSERT INTO julgamento VALUES('83784243673',5,true,'2020-01-11');
+INSERT INTO julgamento VALUES('59119856598',5,false,NULL);
 
---cargo 
---TODO, nao entendi o que fazer com as cadeiras
-INSERT INTO cargo VALUES('presidente',0,'Brasil','pais',200000);
-INSERT INTO cargo VALUES('deputado',0,'Mogi das Cruzes','cidade',12000);
-INSERT INTO cargo VALUES('deputado',0,'Sao Carlos','cidade',21000);
+--INSERT INTO cargo(nome,local,tipoLocal,salario) VALUES('nome','local','tipoLocal',salario); 
+INSERT INTO cargo(nome,local,tipoLocal,salario) VALUES('presidente','Brasil','pais',200000);
+INSERT INTO cargo(nome,local,tipoLocal,salario) VALUES('deputado','Mogi das Cruzes','cidade',12000);
+INSERT INTO cargo(nome,local,tipoLocal,salario) VALUES('deputado','Sao Carlos','cidade',21000);
 
 -- INSERT INTO partido VALUES('nome', 'programa')
 INSERT INTO partido VALUES('Partido dos Idosos', 'Construir asilos na beira da praia, bingo todos finais de semana, etc.');
@@ -100,20 +86,20 @@ INSERT INTO doacao_juridica VALUES(2000000,'90714175000153','22126173135',2020);
 INSERT INTO doacao_juridica VALUES(2500000,'89236358000121','59119856598',2018);
 INSERT INTO doacao_juridica VALUES(1800000,'90043287000120','79548301288',2018);
 
--- INSERT INTO membro_equipe VALUES('membro','candidatura',ano)
-INSERT INTO membro_equipe VALUES('13874257142','81387911600',2020);
-INSERT INTO membro_equipe VALUES('16126941446','81387911600',2020);
-INSERT INTO membro_equipe VALUES('30741724286','22126173135',2020);
-INSERT INTO membro_equipe VALUES('86713773427','22126173135',2020);
-INSERT INTO membro_equipe VALUES('78384158614','68005813724',2019);
-INSERT INTO membro_equipe VALUES('54363931876','80623351358',2019);
-INSERT INTO membro_equipe VALUES('48515177420','12750152470',2021);
-INSERT INTO membro_equipe VALUES('38523575430','79548301288',2018);
+--INSERT INTO equipe VALUES('nome','candidato',ano)
+INSERT INTO equipe VALUES('Equipe de Manuel Manual','81387911600',2020);
+INSERT INTO equipe VALUES('Equipe de Ednaldo Pereira','22126173135',2020);
+INSERT INTO equipe VALUES('Equipe de Gabriela Pereira','68005813724',2019);
+INSERT INTO equipe VALUES('Equipe de Paulo Tanaka','80623351358',2019);
+INSERT INTO equipe VALUES('Equipe de Eduardo Santos','12750152470',2021);
+INSERT INTO equipe VALUES('Equipe de Ana Paula','79548301288',2018);
 
-
---ALGUNS DML
-
---CANDIDATOS COM FICHA SUJA / nao podem se candidatar
-SELECT * 
-FROM INDIVIDUO
-	WHERE ficha_limpa = false
+-- INSERT INTO membro_equipe VALUES('membro','equipe',ano)
+INSERT INTO membro_equipe VALUES('13874257142','Equipe de Manuel Manual',2020);
+INSERT INTO membro_equipe VALUES('16126941446','Equipe de Manuel Manual',2020);
+INSERT INTO membro_equipe VALUES('30741724286','Equipe de Ednaldo Pereira',2020);
+INSERT INTO membro_equipe VALUES('86713773427','Equipe de Ednaldo Pereira',2020);
+INSERT INTO membro_equipe VALUES('78384158614','Equipe de Gabriela Pereira',2019);
+INSERT INTO membro_equipe VALUES('54363931876','Equipe de Paulo Tanaka',2019);
+INSERT INTO membro_equipe VALUES('48515177420','Equipe de Eduardo Santos',2021);
+INSERT INTO membro_equipe VALUES('38523575430','Equipe de Ana Paula',2018);
